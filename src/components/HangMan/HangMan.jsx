fa139c031481c4d1aae4e7a930bf75902270c2c3
Mayur Prajapati <mproyur685@gmail.com>
2025-04-18 01:42:43 +0530

    added hangman images and modal

---
+++ src/components/HangMan/HangMan.jsx
@@ -0,0 +1,15 @@
+import { imageData } from "./HangManUtility";
+
+function HangMan( {step }) {
+    const images = imageData;
+
+    return (
+        <div className="w-[300px] h-[300px]">
+            <image 
+                src={step >= images.length ? images[images.length - 1] : images[step]}
+            />
+        </div>
+    )
+}
+
+export default HangMan;
